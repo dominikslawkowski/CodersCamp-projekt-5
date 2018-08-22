294f8ee67184b1dc3f2fb865a4ca81095cbf7bdd
Dominik <dominik.slawkowski@gmail.com>
2018-08-22 14:54:44 +0200

    Add state and fetching method in class App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//importy komponentów
+import Welcome from './components/Welcome';
+import SearchBar from './components/SearchBar';
+import Category from './components/Category';
+import NewsList from './components/NewsList';
+
+//Klucz potrzebny do pobrania danych z api o wiadomosciach
+const API_KEY = '0cc530dfbcad49d39ee43d21303c2cbc';
+
+
 class App extends Component {
+  
+  //ustawienie state dla klasy App - temat wyszukiwanych wiadomosci i tablie z newsami
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: '',
+      news: []
+    }
+
+    this.newsSearch = this.newsSearch.bind(this)
+    this.newsSearch('sports');
+  }
+
+  newsSearch(term) {
+
+    fetch(`https://newsapi.org/v2/top-headlines?country=pl&category=${term}&apiKey=${API_KEY}`)
+      .then(data => data.json())
+      .then(data => this.setState({
+        theme: term,
+        news: data.articles
+      }))
+      .then(() => console.log(this.state.news));
+
+  }
+
   render() {
     return (
       <div className="App">
