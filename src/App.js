46a480dfc948329bd02d93c039a8c9b299c53c36
Adam Oleszko <adamoleszko44@gmail.com>
2018-08-24 14:11:56 +0200

    Add SearchBar
    Add SearchBar
    Edit App

--- src/App.js
+++ src/App.js
@@ -18,29 +18,35 @@ class App extends Component {
     super(props);
     this.state = {
       theme: '',
-      news: []
+      news: [],
+      searchTerm: ''
     }
 
     this.newsSearch = this.newsSearch.bind(this)
-    this.newsSearch('sports');
+    this.newsSearch('general', '');
   }
 
-  newsSearch(term) {
-
-    fetch(`https://newsapi.org/v2/top-headlines?country=pl&category=${term}&apiKey=${API_KEY}`)
+  newsSearch(term, search) {
+    search = '&q=' + search;
+    fetch(`https://newsapi.org/v2/top-headlines?language=en${search}&category=${term}&apiKey=${API_KEY}`)
       .then(data => data.json())
       .then(data => this.setState({
         theme: term,
-        news: data.articles
+        news: data.articles,
+        searchTerm: search
       }))
-      .then(() => console.log(this.state.news));
+      .then(() => console.log(this.state.news))
+      .then(() => console.log(term))
+      .then(() => console.log(search))
+      .then(() => console.log(`https://newsapi.org/v2/top-headlines?country=uk${search}&category=${term}&apiKey=${API_KEY}`));
 
   }
 
   render() {
+    
     return (
       <div className="App">
-
+        <SearchBar onSearchTermChange={(searchTerm) => this.newsSearch('',searchTerm)}/>
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {searchTerm: ''};
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                value={this.state.searchTerm}
+                onChange={(event) => {this.onInputChange(event.target.value)}} />
+            </div>
+        )
+    }
+
+    onInputChange(searchTerm) {
+        this.setState({searchTerm});
+        this.props.onSearchTermChange(searchTerm);
+    }
+}
+
+    export default SearchBar;
