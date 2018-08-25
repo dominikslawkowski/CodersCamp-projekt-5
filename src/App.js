aedc6ffbe51e1a46aa070ae8c6f8cc67e6f5d793
Kryspinn <33183934+Kryspinn@users.noreply.github.com>
2018-08-25 21:39:57 +0200

    kafelek

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Welcome from './components/Welcome';
 import SearchBar from './components/SearchBar';
 import Category from './components/Category';
 import NewsList from './components/NewsList';
+import News from "./components/News";
+
+
+
 
 //Klucz potrzebny do pobrania danych z api o wiadomosciach
 const API_KEY = '0cc530dfbcad49d39ee43d21303c2cbc';
-
 
 class App extends Component {
   
@@ -19,6 +22,24 @@ class App extends Component {
     this.state = {
       theme: '',
       news: [],
+
+
+      //do wyrrzucenia element bedzie przekazywany do kafelka w NewsLIst na razie tylko do wglądu
+
+      dupa: {
+        source: {
+          id: null,
+          name: "Njherald.com"
+        },
+        author: null,
+        title: "MGM opens Massachusetts casino with a flourish",
+        description:
+          "Massachusetts' first resort casino with expansive gambling and entertainment options opened Friday with a Las Vegas-style flourish",
+        url: "http://www.njherald.com/article/20180824/AP/308249981",
+        urlToImage:
+          "http://www.njherald.com/apps/pbcsi.dll/storyimage/NH/20180824/AP/308249981/AR/0/AR-308249981.jpg&MaxW=500",
+        publishedAt: "2018-08-25T13:21:53Z"
+      },
       searchTerm: ''
     }
 
@@ -39,12 +60,16 @@ class App extends Component {
       .then(() => console.log(search))
 
   }
-
+  
   render() {
     
     return (
       <div className="App">
         <SearchBar onSearchTermChange={this.newsSearch}/>
+
+        {/* te elementy też do wyrzucenia */}
+        <News news={this.state.dupa}/>
+        <News news={this.state.dupa} />        
       </div>
     );
   }
